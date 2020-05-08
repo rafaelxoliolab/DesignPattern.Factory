3afff4c46be9276b3394bc74928b0bf31043187b
rafaelxoliohub <rafa.x.reyes.dev@hotmail.com>
2020-05-07 22:38:20 -0500

    Add project files.

---
+++ Structural.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Factory
+{
+    public static class Structural
+    {
+        public static void Run()
+        {
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}", product.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Common Product class
+    /// </summary>
+    abstract class Product { 
+    }
+
+    class ConcreteProductA:Product { 
+    
+    }
+
+    class ConcreteProductB : Product
+    {
+
+    }
+
+    /// <summary>
+    /// Object Creator class 
+    /// </summary>
+    abstract class Creator {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+}
